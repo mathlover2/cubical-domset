d7edf49a71c4910e1063ae6ef37c16098c0dbeaf
mathlover2 <mathlover2@gmail.com>
2015-12-26 11:59:06 -0500

    Added the function randomGame, which roduces a game via iteratively picking the next move at random until a victory is reached.

--- Game/CubicalDomset/AI.hs
+++ Game/CubicalDomset/AI.hs
@@ -4,7 +4,6 @@ module Game.CubicalDomset.AI
 import Game.CubicalDomset.Gamespace
 import Game.CubicalDomset.Notation
 import Game.CubicalDomset.Rules
-import System.Random
 
 type CubicalDomsetAI = GameRecord -> IO GameRecord
 
@@ -23,11 +22,5 @@ firstMove g = let x = head $ possibleMoves g in return (embedMove x g)
 randomMove :: CubicalDomsetAI
 randomMove g = do x <- randomElement $ possibleMoves g
                   return (embedMove x g)
-
--- The helper function randomElement.
-
-randomElement l = let lnn = length l
-                  in  do r <- randomRIO (0,lnn-1)
-                         return (l !! r)
 
 
--- Game/CubicalDomset/Gamespace.hs
+++ Game/CubicalDomset/Gamespace.hs
@@ -4,26 +4,46 @@ module Game.CubicalDomset.Gamespace
 import Data.Maybe (isNothing)
 import Game.CubicalDomset.Rules
 import Game.CubicalDomset.Notation
+import System.Random
+
+initialGame = GameRecord [start]
 
 successors :: GameRecord -> [GameRecord]
 successors g = map (flip embedMove g) (possibleMoves g)
 
-predecessor = GameRecord . init . getGameRecord 
+predecessor (GameRecord x) = GameRecord (init x)
 
 listOfAllGamesOf :: GameRecord -> [GameRecord]
 listOfAllGamesOf g = analyzeGames g
   where analyzeGames g
           = do x <- successors g
-               if not $ isNothing $ hasVictory x
+               if isFinished x
                  then return x
                  else analyzeGames x
 
 progressionsFrom g = iterate (concat . map successors) [g]
 
-listOfAllGames = listOfAllGamesOf (GameRecord [start])
+listOfAllGames = listOfAllGamesOf initialGame
 
 localImage fwd bwd
   = concat
     . map (\x -> progressionsFrom x !! fwd)
     . return
     . (\x -> iterate predecessor x !! bwd)
+
+gameLength (GameRecord x) = length x
+
+isFinished :: GameRecord -> Bool
+isFinished = not . isNothing . hasVictory
+
+randomGame = build initialGame
+  where build g = if isFinished g
+                  then return g
+                  else do x <- randomElement (successors g)
+                          build x
+
+-- The helper function randomElement.
+
+randomElement l = let lnn = length l
+                  in  do r <- randomRIO (0,lnn-1)
+                         return (l !! r)
